e3b1b4a5c94a82109e7b2419633a799a7c4b68f1
joe <joeykunzler@gmail.com>
2014-04-17 16:02:01 -0500

    Flashlight App Initial Commit
    Messing around with iOS

---
+++ flashViewController.h
@@ -0,0 +1,15 @@
+//
+//  flashViewController.h
+//  Flashlight
+//
+//  Created by Joseph Kunzler on 4/17/14.
+//  Copyright (c) 2014 Joseph Kunzler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface flashViewController : UIViewController
+    
+    @property (nonatomic) BOOL isOn;
+    
+    @end
